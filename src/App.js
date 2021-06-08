0262653e8497094771f4488ac2514b019f35f592
manohar p <manohar2022000@gmail.com>
2021-06-08 17:12:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React from 'react';
-import BellOutlined from'@ant-design/icons';
-import './App.css';import { Layout, Menu, Button } from 'antd';
-
+import React from "react";
+import "./App.css";
+import { Layout, Menu, Button } from "antd";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Api from "./api";
+import {BellOutlined  
+} from '@ant-design/icons';
+import Logo from './logo.png';
+import Form from './functions';
 const { Header, Content, Footer } = Layout;
-class App extends React.Component{
-    render(){
-        return(
-          <Layout className="layout">
-          <Header>
-            <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-              <BellOutlined />
-              <Button type="primary" className="btn">Login</Button>
-            </Menu>
-          </Header>
-          <Content >
-            
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout className="layout">
+          <BrowserRouter>
+            <Header>
+              <div className="logo" ><img src={Logo} alt="logo"></img></div>
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+                <Menu.Item key="1" >
+                  <Link to="/">Tutorial</Link>
+                </Menu.Item>
+                <Menu.Item key="2" >
+                  <Link to="/nav2">News</Link>
+                </Menu.Item>
+                <Menu.Item key="3" >
+                  <Link to="/nav3">Blog</Link>
+                </Menu.Item>
 
-            <div className="site-layout-content">
-              content {/*content */}
+                <BellOutlined className="bell"/>
+                <Button type="primary" className="btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+              </Menu>
+            </Header>
+            <Content>
+              <div className="site-layout-content">
+                {/*content */}
+                <Switch>
+                  <Route path="/" exact>
+                    <Api />
+                  </Route>
+                  <Route path="/nav2">
+                    <p>News </p>
+                  </Route>
+                  <Route path="/nav3">
+                    <p>Blog</p>
+                  </Route>
+                  <Route path="/login">
+                    <Form />
+                  </Route>
+                </Switch>
               </div>
-
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Content>
+          </BrowserRouter>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
         </Layout>
-
-        );
-
-    }
+      </div>
+    );
+  }
 }
 export default App;
---
+++ src/api.js
@@ -0,0 +1,47 @@
+
+import React from 'react';
+
+ class Api extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+          items:[],
+          isLoaded:false,
+        }
+      }
+
+      componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then(json => {
+          this.setState({
+            isLoaded:true,
+            items:json,
+          })
+        })  ;
+      }
+    
+    
+    render(){
+        let { isLoaded , items}=this.state;
+        
+        
+          if({isLoaded}){
+              return(
+            <div className="cardspot">
+          
+              {items.map(item=>(
+                
+                <a href=""><div className="card" >{item.id}.{item.title}</div></a>
+                
+              ))}
+            
+          </div>)}
+           else{
+           return(
+        <p>loading..</p>)}
+           
+    }
+
+}
+export default Api;
